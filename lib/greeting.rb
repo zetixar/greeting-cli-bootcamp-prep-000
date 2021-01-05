f47064d089f16264424101aed142cb0d9c745653
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-05 16:54:14 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,8 @@
 # code the #greeting method here!
+def greet
+  puts "Hi! I'm HAL, what's your name?"
+end
 def greeting name
-puts "Hi! I'm HAL, what's your name?"
+
 puts "Hello #{name}. It's nice to meet you."
 end
